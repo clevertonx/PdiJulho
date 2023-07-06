6db74ef0742fb0255da9810922e9a2a0e79ca14d
clevertonx <clevertonx@gmail.com>
2023-07-06 10:48:50 -0400

    lista completa + CurrentMonth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
-const App = () => {
-  return (
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth } from './helpers/dateFilter';
 
+const App = () => {
+  const [list, setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* area de informações */}
+
+        {/* area de inserção */}
+
+        {/* tabela de itens */}
+      </C.Body>
+    </C.Container>
   );
 }
+
+export default App;
---
+++ src/helpers/dateFilter.ts
@@ -0,0 +1,5 @@
+export const getCurrentMonth = () => {
+    let now = new Date();
+    return `${now.getFullYear()}-${now.getMonth()+1}`;
+
+}
